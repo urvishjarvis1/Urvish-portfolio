e08f280ad7dfea05d42328467f6e6cf29df45547
urvish <urvishrana3728@gmail.com>
2026-02-27 12:01:06 -0500

    fixing pipeline
    Signed-off-by: urvish <urvishrana3728@gmail.com>

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/urvish-portfolio/',
+  // Use relative base so the site works regardless of repo name casing or subpath
+  base: './',
   plugins: [react()],
 })
